ca1af41edbf03706101771e13babc9eabd5a12af
Alona_Pishchulova <pischulova@gmail.com>
2015-03-31 17:56:59 +0300

    transactional manager added

--- src/main/java/ua/epam/rd/repository/JPAPizzaRepository.java
+++ src/main/java/ua/epam/rd/repository/JPAPizzaRepository.java
@@ -1,12 +1,15 @@
 package ua.epam.rd.repository;
 
+import org.springframework.stereotype.Repository;
 import ua.epam.rd.domain.Pizza;
 import ua.epam.rd.domain.PizzaType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository("pizzaRepository")
 public class JPAPizzaRepository implements PizzaRepository {
     EntityManagerFactory emf;
 
@@ -14,6 +17,7 @@ public class JPAPizzaRepository implements PizzaRepository {
         this.emf = emf;
     }
 
+    @Transactional
     @Override
     public Long savePizza(Pizza pizza) {
         EntityManager em = emf.createEntityManager();
